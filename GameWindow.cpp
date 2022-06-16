2c2ac50c06b656093c70f83bd32d702298e6d327
Laidym <PaulLiu831@gmail.com>
2022-06-16 13:02:33 +0800

    intro video at opening

--- GameWindow.cpp
+++ GameWindow.cpp
@@ -9,6 +9,10 @@ const char *title = "Final Project Team-4";
 ALLEGRO_DISPLAY* display = NULL;
 ALLEGRO_SAMPLE *intro=NULL;
 ALLEGRO_SAMPLE_INSTANCE *sample_instance;
+ALLEGRO_VIDEO *video;
+static char const *filename;
+ALLEGRO_EVENT_QUEUE *queue;
+ALLEGRO_EVENT event;
 
 int Game_establish() {
     int msg = 0;
@@ -62,7 +66,74 @@ void game_init() {
     //e_monster = ( Mon * )malloc( sizeof( Mon ) );
 }
 
+void init_video(){
+
+    al_init_video_addon();
+
+    //al_set_new_display_flags(ALLEGRO_RESIZABLE);
+    //al_set_new_display_option(ALLEGRO_VSYNC, 1, ALLEGRO_SUGGEST);
+    //screen = al_create_display( 640 , 480 );
+    // linear interpolation for scaling images
+    al_set_new_bitmap_flags(ALLEGRO_MIN_LINEAR | ALLEGRO_MAG_LINEAR);
+    filename = "intro.ogv";
+    printf("reading video.....\n");
+    video = al_open_video(filename);
+    if( video )
+        printf("read video succeed\n");
+    else
+        printf("read video fail!!!!\n");
+    queue = al_create_event_queue();
+    // register video event
+    ALLEGRO_EVENT_SOURCE *temp = al_get_video_event_source(video);
+    al_register_event_source(queue, temp);
+    al_register_event_source(queue, al_get_display_event_source(display));
+    al_register_event_source(queue, al_get_timer_event_source(fps));
+
+}
+
+void video_display(ALLEGRO_VIDEO *video) {
+    ALLEGRO_BITMAP *frame = al_get_video_frame(video);
+    // check if we get the frame successfully
+    // Note the this code is necessary
+    if ( !frame )
+        return;
+    // Display the frame.
+    // rescale the frame into the size of screen
+    al_draw_scaled_bitmap(frame,
+                          // the rescale position of the frame
+                          0, 0,
+                          // the width and height you want to rescale
+                          al_get_bitmap_width(frame),
+                          al_get_bitmap_height(frame),
+                          // the position of result image
+                          0, 0,
+                          // the width and height of result image
+                          al_get_display_width(display),
+                          al_get_display_height(display), 0);
+    al_flip_display();
+}
+
+void video_begin(){
+    al_reserve_samples(1);
+    al_start_video(video, al_get_default_mixer());
+    al_start_timer(fps);
+}
+
 void game_begin() {
+    init_video();
+    video_begin();
+    while( 1 ){
+        al_wait_for_event(queue, &event);
+        if( event.type == ALLEGRO_EVENT_TIMER ) {
+            video_display(video);
+        } else if( event.type == ALLEGRO_EVENT_DISPLAY_CLOSE ) {
+            al_close_video(video);
+            break;
+        } else if( event.type == ALLEGRO_EVENT_VIDEO_FINISHED ) {
+            break;
+        }
+    }
+    al_destroy_event_queue(queue);
     // Load sound
     intro = al_load_sample("./sound/overworld.mp3");
     al_reserve_samples(20);
@@ -74,7 +145,7 @@ void game_begin() {
     // set the volume of instance
     al_set_sample_instance_gain(sample_instance, 1 );
     al_play_sample_instance(sample_instance);
-    al_start_timer(fps);
+    //al_start_timer(fps);
     // initialize the menu before entering the loop
     menu_init();
 }
